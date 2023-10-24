01c867e1c319e7fbe7876523d8fa109a13e0734d
nagarjuna-madluri <nagarjuna.madluri@gmail.com>
2023-10-24 15:26:09 +0530

    Dispatch functionality

--- src/components/Contacts/Contacts.js
+++ src/components/Contacts/Contacts.js
@@ -1,11 +1,42 @@
-import React from "react";
+import React, { useState } from "react";
 import "./Contacts.css";
-import contactsInitialState from "../../data/contacts";
 import "../../store";
-import { useSelector } from "react-redux";
-
+import { useSelector, useDispatch } from "react-redux";
+import contactsSlice from "../../slices/contacts";
+import { v1 as uuid } from "uuid";
 function Contacts() {
+  let [firstName, setFirstName] = useState("");
+  let [lastName, setLastName] = useState("");
+  let [email, setEmail] = useState("");
+  let [phone, setPhone] = useState("");
   let contacts = useSelector((state) => state.contacts);
+
+  /* Dipatch used to carry the actions to redcers */
+  let dispatch = useDispatch();
+
+  /* Add the contacts using the dispatch */
+  let onAddClick = () => {
+    dispatch(
+      contactsSlice.actions.add({
+        id: uuid(),
+        firstName,
+        lastName,
+        email,
+        phone,
+      })
+    );
+    setFirstName("");
+    setLastName("");
+    setEmail("");
+    setPhone("");
+  };
+
+  /* Delete the contacts using the dispatch */
+  let onDeleteClick = (contact) => {
+    if (window.confirm("Are you sure you want to Delete contact ?")) {
+      dispatch(contactsSlice.actions.remove(contact.id));
+    }
+  };
   return (
     <div className="container">
       <h4 className="grid-header">Contacts</h4>
@@ -19,6 +50,8 @@ function Contacts() {
               type="text"
               placeholder="First Name"
               className="form-control"
+              value={firstName}
+              onChange={(event) => setFirstName(event.target.value)}
             />
           </div>
 
@@ -27,18 +60,34 @@ function Contacts() {
               type="text"
               placeholder="Last Name"
               className="form-control"
+              value={lastName}
+              onChange={(event) => setLastName(event.target.value)}
             />
           </div>
 
           <div className="form-group">
-            <input type="text" placeholder="Email" className="form-control" />
+            <input
+              type="text"
+              placeholder="Email"
+              className="form-control"
+              value={email}
+              onChange={(event) => setEmail(event.target.value)}
+            />
           </div>
 
           <div className="form-group">
-            <input type="text" placeholder="Phone" className="form-control" />
+            <input
+              type="text"
+              placeholder="Phone"
+              className="form-control"
+              value={phone}
+              onChange={(event) => setPhone(event.target.value)}
+            />
           </div>
 
-          <button className="button button-green">Save</button>
+          <button className="button button-green" onClick={() => onAddClick()}>
+            Save
+          </button>
         </details>
       </div>
 
@@ -64,7 +113,14 @@ function Contacts() {
                 <td>{contact.email}</td>
                 <td>{contact.phone}</td>
                 <td>
-                  <button className="button button-red">Delete</button>
+                  <button
+                    className="button button-red"
+                    onClick={() => {
+                      onDeleteClick(contact);
+                    }}
+                  >
+                    Delete
+                  </button>
                 </td>
               </tr>
             ))}
